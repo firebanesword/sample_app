c927440c5865c29ecbcb5cad5a7102e70088e860
firebanesword <mike.togtman@gmail.com>
2012-11-14 18:55:33 -0500

    9.3.1

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -14,14 +14,17 @@ describe "Authentication" do
     describe "signin" do
     before { visit signin_path }
 
-     describe "with valid information" do
+         describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
       before { sign_in user }
 
       it { should have_selector('title', text: user.name) }
+
+      it { should have_link('Users',    href: users_path) }
       it { should have_link('Profile',  href: user_path(user)) }
       it { should have_link('Settings', href: edit_user_path(user)) }
       it { should have_link('Sign out', href: signout_path) }
+
       it { should_not have_link('Sign in', href: signin_path) }
     
 	
@@ -80,6 +83,11 @@ describe "Authentication" do
         describe "submitting to the update action" do
           before { put user_path(user) }
           specify { response.should redirect_to(signin_path) }
+        end
+		
+		 describe "visiting the user index" do
+          before { visit users_path }
+          it { should have_selector('title', text: 'Sign in') }
         end
       end
 	  
