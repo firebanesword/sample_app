0033b8e4d3aa59996bde9ee00d99a9018b7d689e
firebanesword <mike.togtman@gmail.com>
2012-11-14 10:55:45 -0500

    6.3.2

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,8 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+						password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
@@ -22,7 +23,9 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
-  
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
   it { should be_valid }
 
   describe "when name is not present" do
@@ -63,5 +66,20 @@ describe User do
     end
 
     it { should_not be_valid }
+  end
+  
+  describe "when password is not present" do
+  before { @user.password = @user.password_confirmation = " " }
+  it { should_not be_valid }
+  end
+  
+  describe "when password doesn't match confirmation" do
+  before { @user.password_confirmation = "mismatch" }
+  it { should_not be_valid }
+  end
+  
+  describe "when password confirmation is nil" do
+  before { @user.password_confirmation = nil }
+  it { should_not be_valid }
   end
 end
