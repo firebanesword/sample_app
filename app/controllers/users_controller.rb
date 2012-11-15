e6bad433ab0d6be790c3e9c71156021ede00b17a
firebanesword <mike.togtman@gmail.com>
2012-11-14 19:33:05 -0500

    9.4

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,	 only: :destroy
 
   def show
     @user = User.find(params[:id])
@@ -15,6 +16,12 @@ class UsersController < ApplicationController
   
   def edit
     @user = User.find(params[:id])
+  end
+  
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
   end
   
   def update
@@ -38,7 +45,7 @@ class UsersController < ApplicationController
     end
   end
    
-    private
+  private
 
     def signed_in_user
       unless signed_in?
@@ -51,4 +58,8 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
       redirect_to(root_path) unless current_user?(@user)
     end
+	
+	def admin_user
+	  redirect_to(root_path) unless current_usre.admin?
+	end
 end
